4e1e4ad2ad21c2b858b644be547200c66d529c92
unknown <phumsirisumativit@gmail.com>
2024-05-08 00:06:00 +0700

    fix:Q8

--- Question08.scala
+++ Question08.scala
@@ -1,17 +1,37 @@
 object Question08 {
 
-  def getSum(list:List[Int]):Int={
-    if (list.isEmpty) 0
-    else list.head+getSum(list.tail)
+  def getbyindex(list:List[Int],index:Int,current:Int):Int={
+    if (list.isEmpty) return 0
+    if (index==current) return list.head
+    getbyindex(list.tail,index, current+1)
   }
 
-  def sumAll(lists:List[List[Int]]) :List[Int] = {
-    if (lists.isEmpty) List()
-    else getSum(lists.head)::sumAll(lists.tail)
+  def getSumByindex(lists:List[List[Int]],index:Int,currentSum:Int):Int={
+    if (lists.isEmpty) return currentSum
+    getSumByindex(lists.tail,index, currentSum+getbyindex(lists.head,index,0))
+  }
+
+  def getmaxsize(currentmax:Int,lists:List[List[Int]]):Int={
+    if (lists.isEmpty) return currentmax
+    if (lists.head.size>currentmax) return getmaxsize(lists.head.size,lists.tail)
+    return getmaxsize(currentmax,lists.tail)
+  }
+
+  def sumAllOperate(lists:List[List[Int]],currentidx:Int,resultidx:Int) :List[Int] = {
+    if (currentidx>=resultidx) return List()
+    return getSumByindex(lists,currentidx,0)::sumAllOperate(lists,currentidx+1,resultidx)
+  }
+
+  def sumAll(lists: List[List[Int]]):List[Int]={
+    sumAllOperate(lists,0,getmaxsize(0,lists))
   }
 
   def main(args:Array[String]) :Unit={
-    println(sumAll(List(List(1,2,3,4,5),List(),List(4,5,0),List(5))))
+    println(sumAll(List()))
+    println(sumAll(List(List())))
+    println(sumAll(List(List(1,2,3,4),List(),List(4,5),List(1,2,3,4,5,6))))
+    println(sumAll(List(List(3,4),List(1,2,3,4,5,6),List(1,2,3,4))))
+    println(sumAll(List(List(1,2,3,4,5,6),List(1,2,3,4),List(1,2),List(0,0,0,0,0,0,0,0,9))))
   }
 
 }
